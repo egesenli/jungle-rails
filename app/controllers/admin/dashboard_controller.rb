b95ffe04f36d4abd35af0616cb59b1a31f30ccd3
egesenli <egesenli@gmail.com>
2023-04-17 23:05:25 -0400

    Fix variable name error

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_NAME']
+  http_basic_authenticate_with name: ENV['USER_NAME'], password: ENV['USER_PASSWORD']
   def show
   end
 end
